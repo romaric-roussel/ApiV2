c6a72618049b7db299220fe5b3bdf58c3305f903
romaric roussel <romaric.roussel@outlook.fr>
2019-02-13 22:15:10 +0100

    refactor code

--- app.js
+++ app.js
@@ -4,18 +4,12 @@ const axios = require('axios')
 const bodyParser= require('body-parser')
 const app = express()
 const Pokedex = require('pokedex-promise-v2');
-const mysqlAsync = require('mysql')
 
 let P = new Pokedex();
 
 
  //connection mysql
- const connectionAsync = mysqlAsync.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:"",
-    database: 'API'
-  })
+ 
 
  /* const queryString = "SELECT idPlante,id_image,nomFr,nomLatin,url, usageMilieu from plante inner JOIN image on plante.id_image = image.idImage "
   connectionAsync.query(queryString,(err,rows,fields)=> {
@@ -37,75 +31,9 @@ app.use(bodyParser.json())
 app.use(morgan('combined'))
 
 
-app.get("/pokemon", async (req,res)=>{
-    let formatJson
-      try {
-          const allPokemon =  await getAllPokemon()
-          formatJson = {status:"true",count:allPokemon.data.count,result:{data:allPokemon.data.results}}
-          console.log(formatJson.result.data)
-          for(let i = 0;i<20;i++){
-              const onePokemon = await getIdPokemonAndImageFromName(allPokemon.data.results[i].name)
-              console.log(onePokemon.data.id + "kgek")
-              formatJson.result.data[i].id = onePokemon.data.id
-              formatJson.result.data[i].image = onePokemon.data.sprites.front_default
+const pokemonRoute = require("./routes/pokemon.js")
 
-          }
-          res.json(formatJson)
-      }catch (error){
-          res.send(error)
-      }
-     
-    
-})
-
-app.get("/pokemon/:id", async (req,res)=>{
-    try{
-        const pokemon =  await getPokemonById(req.params.id)
-        let formatJson = {
-            status: "true",
-            result: {
-                id_pokemon: pokemon.data.id,
-                nom: pokemon.data.name,            
-                nom_type_1: pokemon.data.types[0].type.name,
-                nom_type_2: pokemon.data.types[1].type.name,
-                url_image: pokemon.data.sprites.front_default
-            }
-        }
-        res.json(formatJson)
-    }catch(error){
-        res.send(error)
-    }
-    
-})
-
-
-const getAllPokemon = async () => {
-    
-    try {
-        return await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
-      } catch (error) {
-        console.error(error)
-      }
-}
-
-const getPokemonById = async (id) => {
-    
-    try {
-        return await axios.get('https://pokeapi.co/api/v2/pokemon/'+id)
-      } catch (error) {
-        console.error(error)
-      }
-}
-
-const getIdPokemonAndImageFromName = async (name) => {
-    
-    try {
-        return await axios.get('https://pokeapi.co/api/v2/pokemon/'+name)
-      } catch (error) {
-        console.error(error)
-      }
-}
-
+app.use(pokemonRoute)
 
     
 
