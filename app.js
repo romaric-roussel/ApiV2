679b552a0de39d91ac86761d6e6bab8d19c6f2ae
romaric roussel <romaric.roussel@outlook.fr>
2019-02-13 17:53:27 +0100

    save: /pokemon/id

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ const axios = require('axios')
 const bodyParser= require('body-parser')
 const app = express()
 const Pokedex = require('pokedex-promise-v2');
+const mysqlAsync = require('mysql')
+
 let P = new Pokedex();
+
+
+ //connection mysql
+ const connectionAsync = mysqlAsync.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:"",
+    database: 'API'
+  })
+
+ /* const queryString = "SELECT idPlante,id_image,nomFr,nomLatin,url, usageMilieu from plante inner JOIN image on plante.id_image = image.idImage "
+  connectionAsync.query(queryString,(err,rows,fields)=> {
+    if (err){
+      console.log("error " + err)
+      res.sendStatus(204)
+      return
+    }
+    const plante = rows.map((row) => {
+      return {idPlante:row.idPlante, nomFr:row.nomFr,nomLatin:row.nomLatin, usageMilieu:row.usageMilieu,
+        image :{idImage:row.id_image,url:row.url}}
+    })
+    res.json(plante)
+  }) */
 
 
 app.use(bodyParser.urlencoded({extended:false}))
