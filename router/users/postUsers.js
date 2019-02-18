7a77414184c3b3d9f41ccede14bab0ec20be16ec
romaric roussel <romaric.roussel@outlook.fr>
2019-02-18 22:35:23 +0100

    feat: /user/login
    post method, login user with email and password

--- router/users/postUsers.js
+++ router/users/postUsers.js
@@ -2,6 +2,9 @@ const express = require('express')
 const userRouterPost = express.Router()
 const connection = require("../../connection")
 const bcrypt = require('bcrypt')
+//const util = require('util')
+const JSON = require('circular-json');
+
 
 
 userRouterPost.post("/user", (req,res)=>{
@@ -29,6 +32,44 @@ userRouterPost.post("/user", (req,res)=>{
     })
     
 })
+
+userRouterPost.post("/user/login", (req,res)=>{
+    const login = req.body.login;
+    const password = req.body.password;
+    
+    loginTest(login,password,res)
+    
+   
+  
+})
+
+const loginTest = (login,password,response) =>{
+  
+    const queryGetMdp = "select mdp from utilisateur where mail = ? "
+    connection.getConnection().query(queryGetMdp,[login],(err,rows,fields)=>{
+            if(err){
+                response.status(500).send(err.message)
+                return
+               
+            }else {
+                bcrypt.compare( password,rows[0].mdp, function(err, res) {
+                    if(res) {
+                     // Passwords match
+                     response.status(200).send("loggin success")
+                     
+                    } else {
+                     // Passwords don't match
+                     response.status(500).send("loggin failed")
+                    } 
+                  });
+            }
+    })
+   
+    
+    
+   
+    
+}
 
 
 module.exports.userRouterPost = userRouterPost
