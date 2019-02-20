99f1ab7e09e056b7978b0313aee3befc8774e64e
romaric roussel <romaric.roussel@outlook.fr>
2019-02-20 12:12:08 +0100

    fix : connexion

--- connection.js
+++ connection.js
@@ -3,10 +3,11 @@ const mysql = require('mysql')
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host: 'localhost',
-    user: 'root',
-    password:"",
-    database: 'api'
+    host: 'db4free.net',
+    user: 'romaric',
+    password:"azeqsdwxc",
+    port:'3307',
+    database: 'apipokemons'
   })
 
  const getConnection  = () => {
