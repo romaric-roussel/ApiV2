e8a75943626765c1bc2000fa36a5b31c21227c67
romaric roussel <romaric.roussel@outlook.fr>
2019-04-08 21:58:23 +0200

    refactor: /user/:id/friends
    refactor json output

--- router/friends/getFriends.js
+++ router/friends/getFriends.js
@@ -4,13 +4,15 @@ const connection = require("../../connection")
 
 
 friendsRouterGet.get("/user/:id/friends", (req,res)=>{
+    let formatJson = {status:"true",result:{data:[]}}
     const query = "SELECT id_utilisateur2 as idAmis ,id_amis as id_liste_amis,nom,prenom,mail,photo from amis LEFT JOIN utilisateur on utilisateur.id_utilisateur = amis.id_utilisateur2 where id_utilisateur1 = ?"
     connection.getConnection().query(query,[req.params.id],(err,rows,fiels)=>{
         if(err){
             res.sendStatus(404)
             return
         }else {
-            res.json(rows)
+            formatJson.result.data = rows
+            res.send(formatJson).statusCode(200)
         }
     })
     
